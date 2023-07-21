a19839b7d2b1d4841c8be93a5c267094061bdc54
Jam <77445181+jamjes@users.noreply.github.com>
2023-07-21 15:02:01 +0100

    //Better GUI and Improved Magnetism
    - flipped attraction and repulsion
    - Using sprites to indicate magnetic field instead of debug rays and placeholder game objects

--- magjump/Assets/_Scripts/PlayerController.cs
+++ magjump/Assets/_Scripts/PlayerController.cs
@@ -21,11 +21,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _extraDistance = 2f;
     private float _horizontalInput;
     private bool _isFacingRight = true;
-    private Vector2 _direction;
+    public Vector2 Direction {private set; get;}
     private Platform _targetPlatform;
-
-    [Header("Other")]
-    public GameObject UpPointer, RightPointer, DownPointer, LeftPointer;
 
     #endregion
 
@@ -41,16 +38,16 @@ public class PlayerController : MonoBehaviour
 
         if (_targetPolarity == Polarity.North && Input.GetButton("NorthMagnet") || _targetPolarity == Polarity.South && Input.GetButton("SouthMagnet"))
         {
-            if (_rigidBody.gravityScale != 0) _rigidBody.gravityScale = 0;
-            _rigidBody.velocity = _direction * _speed;
+            if (_rigidBody.gravityScale != 1) _rigidBody.gravityScale = 1;
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, (Direction.y * -1 * _magneticForce));
+            
 
         }
 
         if (_targetPolarity == Polarity.North && Input.GetButtonDown("SouthMagnet") || _targetPolarity == Polarity.South && Input.GetButtonDown("NorthMagnet"))
         {
-            if (_rigidBody.gravityScale != 1) _rigidBody.gravityScale = 1;
-            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, (_direction.y * -1 * _magneticForce));
-            
+            if (_rigidBody.gravityScale != 0) _rigidBody.gravityScale = 0;
+            _rigidBody.velocity = Direction * _speed;
         }
 
         if (Input.GetButtonUp("NorthMagnet") || Input.GetButtonUp("SouthMagnet"))
@@ -60,74 +57,14 @@ public class PlayerController : MonoBehaviour
 
         float xDirection = Input.GetAxisRaw("Horizontal");
         float yDirection = Input.GetAxisRaw("Vertical");
-        _direction = new Vector2(xDirection, yDirection);
-        DrawDebugRays(_direction);
+        Direction = new Vector2(xDirection, yDirection);
     }
 
-    private void DrawDebugRays(Vector2 direction)
-    {
-        switch(direction)
-        {
-            case Vector2 v when v.Equals(Vector2.up):
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.up * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.up * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + _extraDistance), Vector2.left * (_boxCollider.bounds.extents.x + 0.5f), Color.blue);
-                UpPointer.SetActive(true);
-                RightPointer.SetActive(false);
-                DownPointer.SetActive(false);
-                LeftPointer.SetActive(false);
-                break;
-            
-            case Vector2 v when v.Equals(Vector2.right):
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x - 0.5f, 0.5f), Vector2.right * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x - 0.5f, -0.5f), Vector2.right * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x + _extraDistance, 0.5f), Vector2.down * (_boxCollider.bounds.extents.y + 0.5f), Color.blue);
-                UpPointer.SetActive(false);
-                RightPointer.SetActive(true);
-                DownPointer.SetActive(false);
-                LeftPointer.SetActive(false);
-                break;
-            
-            case Vector2 v when v.Equals(Vector2.down):
-                Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + _extraDistance), Vector2.right * (_boxCollider.bounds.extents.x + 0.5f), Color.blue);
-                UpPointer.SetActive(false);
-                RightPointer.SetActive(false);
-                DownPointer.SetActive(true);
-                LeftPointer.SetActive(false);
-                break;
-            
-            case Vector2 v when v.Equals(Vector2.left):
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x - 0.5f, -0.5f), Vector2.left * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x - 0.5f, 0.5f), Vector2.left * (_boxCollider.bounds.extents.y + _extraDistance), Color.blue);
-
-                Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x + _extraDistance, 0.5f), Vector2.up * (_boxCollider.bounds.extents.y + 0.5f), Color.blue);
-                UpPointer.SetActive(false);
-                RightPointer.SetActive(false);
-                DownPointer.SetActive(false);
-                LeftPointer.SetActive(true);
-                break;
-
-            case Vector2 v when v.Equals(Vector2.zero):
-                UpPointer.SetActive(false);
-                RightPointer.SetActive(false);
-                DownPointer.SetActive(false);
-                LeftPointer.SetActive(false);
-                break;
-        }
-    }
+    
 
     private bool MagnetismCheck()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, _direction, _extraDistance, _groundLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Direction, _extraDistance, _groundLayer);
 
         if (raycastHit.collider != null)
         {
---
+++ magjump/Assets/_Scripts/PlayerSpriteHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteHandler : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private PlayerMagnetismHandler _player;
+    [SerializeField] private Sprite _idle;
+    [SerializeField] private Sprite _upPending;
+    [SerializeField] private Sprite _upNorth;
+    [SerializeField] private Sprite _upSouth;
+    [SerializeField] private Sprite _rightPending;
+    [SerializeField] private Sprite _rightNorth;
+    [SerializeField] private Sprite _rightSouth;
+    [SerializeField] private Sprite _downPending;
+    [SerializeField] private Sprite _downNorth;
+    [SerializeField] private Sprite _downSouth;
+    [SerializeField] private Sprite _leftPending;
+    [SerializeField] private Sprite _leftNorth;
+    [SerializeField] private Sprite _leftSouth;
+
+    private void Update()
+    {
+        switch(_player.Direction)
+        {
+            case Vector2 v when v.Equals(Vector2.zero):
+                if (_spriteRenderer.sprite != _idle) _spriteRenderer.sprite = _idle;
+                break;
+            
+            case Vector2 v when v.Equals(Vector2.up):
+                if (_player.NorthField) _spriteRenderer.sprite = _upNorth;
+                else if (_player.SouthField) _spriteRenderer.sprite = _upSouth;
+                else if (_spriteRenderer.sprite != _upPending) _spriteRenderer.sprite = _upPending;
+                break;
+
+            case Vector2 v when v.Equals(Vector2.right):
+                if (_player.NorthField) _spriteRenderer.sprite = _rightNorth;
+                else if (_player.SouthField) _spriteRenderer.sprite = _rightSouth;
+                else if (_spriteRenderer.sprite != _rightPending) _spriteRenderer.sprite = _rightPending;
+                break;
+
+            case Vector2 v when v.Equals(Vector2.down):
+                if (_player.NorthField) _spriteRenderer.sprite = _downNorth;
+                else if (_player.SouthField) _spriteRenderer.sprite = _downSouth;
+                else if (_spriteRenderer.sprite != _downPending) _spriteRenderer.sprite = _downPending;
+                break;
+
+            case Vector2 v when v.Equals(Vector2.left):
+                if (_player.NorthField) _spriteRenderer.sprite = _leftNorth;
+                else if (_player.SouthField) _spriteRenderer.sprite = _leftSouth;
+                else if (_spriteRenderer.sprite != _leftPending) _spriteRenderer.sprite = _leftPending;
+                break;
+            
+        }
+    }
+}
