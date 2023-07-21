981d97473caccec561c1724984db23b2a402d862
Jam <77445181+jamjes@users.noreply.github.com>
2023-07-21 13:44:33 +0100

    //2 Levels with menu flow
    - added 2 levels that teach gameplay mechanics
    - can cycle through levels and back to main menu

--- magjump/Assets/_Scripts/LoadThisScene.cs
+++ magjump/Assets/_Scripts/LoadThisScene.cs
@@ -16,4 +16,18 @@ public class LoadThisScene : MonoBehaviour
     {
         Application.Quit(); //Exit application when function is called
     }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            StartCoroutine(DelayScene(1.5f));
+        }
+    }
+
+    IEnumerator DelayScene(float value)
+    {
+        yield return new WaitForSeconds(value);
+        SceneManager.LoadScene(SceneName);
+    }
 }
